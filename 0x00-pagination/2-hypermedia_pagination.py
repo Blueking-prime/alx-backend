b0a46ecce477ca9cefd2b065b5318f5418b534f6
Nnamdi Anyanwu <blueking189@gmail.com>
2023-02-18 17:19:43 +0100

    Add invalid page check

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -7,6 +7,8 @@ from typing import Dict, List, Union
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     '''return a tuple of size two containing a start index and an end index'''
+    if page < 1:
+        return (0, 0)
     return ((page - 1) * page_size, page * page_size)
 
 
