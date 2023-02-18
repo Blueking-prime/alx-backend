0e591f2b43714cea10cfa2c9613bb9df0eafb7f4
Nnamdi Anyanwu <blueking189@gmail.com>
2023-02-18 17:25:11 +0100

    Change typing system

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 '''Returns page index ranges'''
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''return a tuple of size two containing a start index and an end index'''
     if page < 1:
         return (0, 0)
